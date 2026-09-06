9809552a7d71a778127ce39447e679349335ebf4
Anick <anisurrahmananick@gmail.com>
2026-09-06 23:16:58 +0600

    commit

---
+++ src/Components/HeroSec.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const HeroSec = () => {
+  return (
+    <div>
+
+
+        
+    </div>
+  )
+}
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
+
+
+
+
+
 export const NavBar = () => {
   return (
     <>
 
-  
+
+
+
+
     </>
   )
 }
